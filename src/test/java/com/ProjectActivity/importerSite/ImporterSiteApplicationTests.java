7a341b940c460e204ee4cd6663085067b8e0ea25
MrJaymun <alexerm71@mail.ru>
2022-04-13 16:20:37 +0300

    product with added db and created test methods

---
+++ src/test/java/com/ProjectActivity/importerSite/ImporterSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProjectActivity.importerSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImporterSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
